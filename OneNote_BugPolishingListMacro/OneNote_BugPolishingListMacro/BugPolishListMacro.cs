06c711bc6bf7153e569bca29c97b7adc0d1ec6df
Byungsung Park <outofthepark101@gmail.com>
2023-11-28 23:43:45 +0900

    aa

---
+++ OneNote_BugPolishingListMacro/OneNote_BugPolishingListMacro/BugPolishListMacro.cs
@@ -0,0 +1,8 @@
+﻿using GraphBasics;
+
+string tenantId = "fc6c25a4-0c67-4283-b2c8-edf42f343d5c";
+string clientId = "1602dabe-7df3-48f1-8a23-9dbb6445742f";
+string clientSecret = "e5e0d518-583d-4374-9412-db6234acab73";
+
+var graphHandler = new GraphHandler(tenantId, clientId, clientSecret);
+
---
+++ OneNote_BugPolishingListMacro/OneNote_BugPolishingListMacro/OneNote_GraphHandler.cs
@@ -0,0 +1,35 @@
+﻿using Azure.Identity;
+using Microsoft.Graph;
+using Microsoft.Graph.Models;
+using Microsoft.Graph.Models.ExternalConnectors;
+using System.ComponentModel.Design;
+
+namespace GraphBasics;
+
+public class GraphHandler
+{
+    public GraphServiceClient GraphClient { get; set; }  
+
+    public GraphHandler(string tenantId, string clientId,  string clientSecret)
+    {
+        GraphClient = CreateGraphClient(tenantId, clientId, clientSecret);
+    }
+
+    public GraphServiceClient CreateGraphClient(string tenantId, string clientId, string clientSecret)
+    {
+        var options = new TokenCredentialOptions
+        {
+            AuthorityHost = AzureAuthorityHosts.AzurePublicCloud
+        };
+
+        var clientSecretCredential = new ClientSecretCredential(tenantId, clientId, clientSecret, options);
+        var scopes = new[] { "https://graph.microsoft.com/.default" };
+
+        return new GraphServiceClient(clientSecretCredential, scopes);
+    }
+
+    public async Task<User?> GetUser(string userPrincipalName)
+    {
+        return await GraphClient.Users[userPrincipalName].GetAsync();
+    }
+}
